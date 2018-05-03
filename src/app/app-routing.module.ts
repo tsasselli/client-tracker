113f0f40df556af5896bc45467ff98b876979563
tsasselli <tsasselli@gmail.com>
2018-05-03 12:02:53 -0700

    added routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes, Router } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { SettingsComponent } from './components/settings/settings.component';
+
+const routes: Routes = [
+  { path: '', component: DashboardComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'client/add', component: AddClientComponent},
+  { path: 'client/edit/:id', component: EditClientComponent},
+  { path: 'client/:id', component: ClientDetailsComponent},
+  { path: 'settings', component: SettingsComponent},
+  { path: '**', component: NotFoundComponent},
+]
+
+@NgModule({
+  exports: [RouterModule],
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
